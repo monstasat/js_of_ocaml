f4a0eb99070369facecd7afbe4ce4685fe963be3
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-01-06 10:15:40 +0800

    rebase

--- toplevel/lib/jsooTop.mli
+++ toplevel/lib/jsooTop.mli
@@ -31,3 +31,31 @@ val execute : bool -> ?pp_code:formatter -> ?highlight_location:(Location.t -> u
 
 (** Initialize Js_of_ocaml toplevel. *)
 val initialize : unit -> unit
+
+module Wrapped : sig
+
+  (** A [result] type for all the toplevel functions. *)
+  type 'a result =
+    | Success of 'a * warning list
+    | Error of error * warning list
+
+  and error =
+    { msg: string;
+      locs: loc list;
+      if_highlight: string; }
+
+  and warning = error
+
+  and loc = {
+    loc_start: int * int;
+    loc_end: int * int;
+  }
+
+  include JsooTopIntf.Wrapped
+    with type toplevel := unit
+     and type 'a result := 'a result
+     and type output := Format.formatter
+
+  val error_of_exn: exn -> error
+
+end
